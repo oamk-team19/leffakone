457749e59e8e4599489be5918bf9e402de0863c7
PaulaKaa <c3kapa03@students.oamk.fi>
2025-09-08 13:55:03 +0300

    basic signin

--- frontend/src/views/Login/index.jsx
+++ frontend/src/views/Login/index.jsx
@@ -1,6 +1,38 @@
 import * as React from 'react';
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, TextField, Link } from '@mui/material';
 
 export const Login = () => {
-  return <Typography variant="h4">login page</Typography>;
+  return (
+    <>
+      <Typography variant="h4">Login</Typography>
+      <div>
+        <TextField
+          id="outlined-email-input"
+          label="Email"
+          type="email"
+        />
+      </div>
+      <div>
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+        />
+      </div>
+      <div>
+        <Button variant="contained">Sign in</Button>
+      </div>
+      <Link
+        component="button"
+        variant="body2"
+        onClick={() => {
+          console.info("I'm a button.");
+        }}
+      >
+        Don't have an account? Sign up!
+      </Link>
+    </>
+
+  );
+
 };

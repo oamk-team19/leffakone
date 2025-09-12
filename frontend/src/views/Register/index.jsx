88f092d27466a5bd2b796f6f454e730db96c4fe5
Johannes Typpö <t3tyjo01@students.oamk.fi>
2025-09-12 13:51:35 +0300

    Registration UI

--- frontend/src/views/Register/index.jsx
+++ frontend/src/views/Register/index.jsx
@@ -1,6 +1,17 @@
 import * as React from 'react';
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, TextField, Box, Link } from '@mui/material';
 
 export const Register = () => {
-  return <Typography variant="h4">Register page</Typography>;
+  return (
+    <Box display={'flex'} flexDirection={'column'} alignItems={'center'} gap={2}>
+      <Typography variant="h4">Registration</Typography>
+      <Typography variant="body1">Please fill out the form:</Typography>
+      <TextField id="email" label="Email" variant="outlined"/>
+      <TextField id="username" label="Username" variant="outlined" />
+      <TextField id="password" label="Password" variant="outlined" />
+      <TextField id="repeat-password" label="Repeat Password" variant="outlined" />
+      <Button variant="contained">Submit</Button>
+      <Link href="/login">Already signed up? Sign in!</Link>
+    </Box>
+  );
 };

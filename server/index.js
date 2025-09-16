2c47bc701d2298b664f8af8f1fb5cb35788ddd06
PaulaKaa <c3kapa03@students.oamk.fi>
2025-09-16 14:21:47 +0300

    trail

--- server/index.js
+++ server/index.js
@@ -13,13 +13,14 @@ const port = process.env.PORT || 3001;
 const dbpool = pool;
 await dbMigrate();
 const app = express();
-app.use(cors());
+app.use(cors("tesri"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', rootRouter);
 
 app.use('/movies', moviesRouter);
 app.use('/auth', authRouter); //for signin and signup
+
 
 app.use((err, req, res, next) => {
   const status = err.status || 500;

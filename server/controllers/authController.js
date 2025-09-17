6af776f658ef4bea145535cdcb885771e8eaa74b
Johannes Typpö <t3tyjo01@students.oamk.fi>
2025-09-17 14:17:02 +0300

    refreshtoken created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,24 +8,28 @@ const JWT_SECRET = process.env.JWT_SECRET;
 export const signin = async (req, res) => {
   try {
     const { email, password } = req.body; //email and password inside of curly brackets means that it searches them from req.body
-    
-    if(!email || !password){
-      console.log('Missing email and/or password')
-      return res.status(400).json({ message: 'Check email and password'})
+
+    if (!email || !password) {
+      console.log('Missing email and/or password');
+      return res.status(400).json({ message: 'Check email and password' });
     }
 
     const result = await dataForSignIn(email); //send email to db to get a password linked to email
     //console.log(result.data); //result.data = 123
 
-    const match = await bcrypt.compare(password, result.data)
+    const match = await bcrypt.compare(password, result.data);
     if (match) {
       //if (password === result.data) {
       //if passwords match --> create a token
-      const token = jwt.sign({ user: email }, JWT_SECRET, { expiresIn: '2m' }) //expires in 2 minutes
-      return res.status(200).json({ email: email, token: token })
+      return res
+        .exposeHeaders()
+        .authorizationHeader(email)
+        .refreshToken(email)
+        .status(200)
+        .json({ email: email });
     } else {
       //no matching passwords
-      return res.status(401).json({ error: 'Invalid credentials' })
+      return res.status(401).json({ error: 'Invalid credentials' });
     }
   } catch (error) {
     console.error('Not matching passwords', error);
--- server/index.js
+++ server/index.js
@@ -5,22 +5,54 @@ import { dbMigrate, pool } from './helpers/db.js';
 import rootRouter from './routers/rootRouter.js';
 import moviesRouter from './routers/movieRouter.js';
 import authRouter from './routers/authRouter.js';
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
 const env = process.env.NODE_ENV;
 const port = process.env.PORT || 3001;
+const jwt_secret = process.env.JWT_SECRET;
 const dbpool = pool;
 await dbMigrate();
 const app = express();
-app.use(cors("tesri"));
+app.use(cors({ credentials: true, origin: 'http://localhost:5173/' }));
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', rootRouter);
 
+app.use((req, res, next) => {
+  const { sign } = jwt;
+  res.exposeHeaders = () => {
+    return res.header(
+      'Access-Control-Expose-Headers',
+      'Authorization,Set-Cookie'
+    );
+  };
+
+  res.authorizationHeader = (email) => {
+    const access_token = jwt.sign({ user: email }, jwt_secret, {
+      expiresIn: '1m',
+    });
+    return res.header('Authorization', 'Bearer ' + access_token);
+  };
+
+  res.refreshToken = (email) => {
+    const refresh_token = jwt.sign({ user: email }, jwt_secret, {
+      expiresIn: '2m',
+    });
+
+    return res.cookie('refreshToken', refresh_token, {
+      maxAge: 1000 * 60 * 2,
+    });
+  };
+
+  next();
+});
+
+app.use('/', rootRouter);
 app.use('/movies', moviesRouter);
 app.use('/auth', authRouter); //for signin and signup
-
 
 app.use((err, req, res, next) => {
   const status = err.status || 500;

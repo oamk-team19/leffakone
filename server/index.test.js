ea9968ad6d8831bb9fdf5ab2a197e999bbf895ec
PaulaKaa <c3kapa03@students.oamk.fi>
2025-09-27 10:32:27 +0300

    models for testing

---
+++ server/index.test.js
@@ -0,0 +1,21 @@
+import { expect } from "chai"
+
+describe("Testing basic database functionality", () => {
+    it("should login", async () => {
+      
+    })
+
+    it("should register", async () => {
+        //code
+    })
+
+
+    it("should delete register", async () => {
+        //code
+    })
+
+
+    it("should scroll reviews", async () => {
+        //code
+    })
+})

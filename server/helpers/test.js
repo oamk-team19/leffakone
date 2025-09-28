61cff090074d7e8634140560d05d5f69bac4bfb3
PaulaKaa <c3kapa03@students.oamk.fi>
2025-09-28 16:26:26 +0300

    edited scripts and added test.js

---
+++ server/helpers/test.js
@@ -0,0 +1,9 @@
+//To ensure, that database structure is correct, and data is as expected, database is initialized before running tests.
+import fs from 'fs'
+import path from 'path'
+import { pool } from './db.js'
+
+/*
+
+
+*/
--- server/index.test.js
+++ server/index.test.js
@@ -1,20 +1,24 @@
 import { expect } from "chai"
 
-describe("Testing basic database functionality", () => {
+describe("Testing user management", () => {
     it("should login", async () => {
-      
+
+    })
+
+    it("should log out", async () => {
+        //code
     })
 
     it("should register", async () => {
         //code
     })
 
-
-    it("should delete register", async () => {
+    it("should delete registration", async () => {
         //code
     })
+})
 
-
+describe("Testing basic database functionality", () => {
     it("should scroll reviews", async () => {
         //code
     })

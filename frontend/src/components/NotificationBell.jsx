24b0862e907cce32de1a35cac866247fe2c6afd6
PaulaKaa <c3kapa03@students.oamk.fi>
2025-10-05 21:04:25 +0300

    added yes and no button to menu

--- frontend/src/components/NotificationBell.jsx
+++ frontend/src/components/NotificationBell.jsx
@@ -1,13 +1,27 @@
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Badge from '@mui/material/Badge';
 import ButtonIcon from '@mui/material/IconButton';
+import { Menu, MenuItem, Button } from '@mui/material';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useUser } from '../context/useUser'; //get id
+import * as React from 'react';
+import CheckIcon from '@mui/icons-material/Check';
+import NotInterestedIcon from '@mui/icons-material/NotInterested';
 
 export function NotificationsBell() {
-  const { user, setUser, LogOut } = useUser();
+  const { user } = useUser();
   const [pendingStatus, setpendingStatus] = useState(0);
+  const [pendingsArray, setPendingsArray] = useState([]);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   //Trial for notification bell
   useEffect(() => {
@@ -19,19 +33,19 @@ export function NotificationsBell() {
           { idUser: user.id }
         );
 
-        console.log(response);
+        //console.log(response.data);
         //console.log(response.data[0].username);
         //console.log(response.data[1].groupname);
+        setPendingsArray(response.data);
+
+        //console.log(pendingsArray);
 
         //Huomioi aika
-        if (pendingStatus < data.length) {
-          console.log('New pending request');
-          setpendingStatus(data.length);
+        if (pendingStatus < response.data.length) {
+          //console.log('New pending request');
+          setpendingStatus(response.data.length);
           //näytä uusi pyyntö
         }
-        //console.log(response.data);
-
-        //console.log(response.data.length);
       } catch (error) {
         console.log(error);
       }
@@ -44,11 +58,24 @@ export function NotificationsBell() {
   }, []);
 
   return (
-    <ButtonIcon onClick={() => alert('Tätä nappia ei ole vielä toteutettu')}>
-      <Badge color="error" badgeContent={pendingStatus}>
-        {' '}
-        <NotificationsIcon />
-      </Badge>
-    </ButtonIcon>
+    <>
+      <ButtonIcon onClick={handleClick}>
+        <Badge color="error" badgeContent={pendingStatus}>
+          <NotificationsIcon />
+        </Badge>
+      </ButtonIcon>
+
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+        <MenuItem>
+          tekstiä{' '}
+          <ButtonIcon>
+            <CheckIcon />
+          </ButtonIcon>
+          <ButtonIcon>
+            <NotInterestedIcon />
+          </ButtonIcon>
+        </MenuItem>
+      </Menu>
+    </>
   );
 }
